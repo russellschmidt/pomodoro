68c2f13c33a180907cdbd21bb17ef7429f91e517
Russell Schmidt <russell.schmidt@me.com>
2016-05-01 12:21:00 -0700

    add templates

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,26 @@
-angular.module('pomodoro', []);
+(function() {
+  function config($stateProvider, $locationProvider) {
+    
+    // disable hash-bang mode on URLs, and disable a common $location error
+    $locationProvider
+      .html5mode({
+        enabled: true,
+        requireBase: false
+    });
+    
+    $stateProvider
+      .state('landing', {
+        url: '/',
+      templateUrl: '/templates/landing.html'
+      })
+      .state('tomato', {
+        url: '/tomato',
+        templateUrl: 'templates/tomato.html'
+      });
+  }
+  
+  angular
+    .module('pomodoro', ['ui.router'])
+    .config(config);
+})();
+
